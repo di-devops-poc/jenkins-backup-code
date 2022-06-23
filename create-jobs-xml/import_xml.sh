3a391bbf6cda9f136097fea537ae586edd0e0021
di-devops-poc <106592300+di-devops-poc@users.noreply.github.com>
2022-06-23 12:17:40 +0530

    Update import_xml.sh

--- create-jobs-xml/import_xml.sh
+++ create-jobs-xml/import_xml.sh
@@ -2,10 +2,15 @@
 cd
 WORK_SPACE=$(pwd)
 cd ${WORK_SPACE}/jenkins-backup-code/pipeline_xml_backup
-java -jar jenkins-cli.jar -s http://VM_IP_ADDR:8080/ -auth admin:PASS create-job update_application < update_application.xml 
 
-java -jar jenkins-cli.jar -s http://VM_IP_ADDR:8080/ -auth admin:PASS create-job create_all_services < create_all_services.xml
+VM_IP_ADDRESS=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
-java -jar jenkins-cli.jar -s http://VM_IP_ADDR:8080/ -auth admin:PASS create-job create_kubernetes_cluster < create_kubernetes_cluster.xml 
+JENKINS_PASS=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 
-java -jar jenkins-cli.jar -s http://VM_IP_ADDR:8080/ -auth admin:PASS import-credentials-as-xml "system::system::jenkins" < git_cred.xml
+java -jar jenkins-cli.jar -s http://${VM_IP_ADDRESS}:8080/ -auth admin:${JENKINS_PASS} create-job update_application < update_application.xml 
+
+java -jar jenkins-cli.jar -s http://${VM_IP_ADDRESS}:8080/ -auth admin:${JENKINS_PASS} create-job create_all_services < create_all_services.xml
+
+java -jar jenkins-cli.jar -s http://${VM_IP_ADDRESS}:8080/ -auth admin:${JENKINS_PASS} create-job create_kubernetes_cluster < create_kubernetes_cluster.xml 
+
+java -jar jenkins-cli.jar -s http://${VM_IP_ADDRESS}:8080/ -auth admin:${JENKINS_PASS} import-credentials-as-xml "system::system::jenkins" < git_cred.xml
