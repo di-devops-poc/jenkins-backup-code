bac4962260b283485b583c945059dbeed9b701a2
di-devops-poc <106592300+di-devops-poc@users.noreply.github.com>
2022-06-23 11:49:25 +0530

    Create update_import_xml.sh

---
+++ create-jobs-xml/update_import_xml.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd jenkins-backup-code/create-jobs-xml
+
+
+VM_IP_ADDRESS=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+JENKINS_PASS=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+
+
+sed -e 's%VM_IP_ADDR%'"${VM_IP_ADDRESS}"'%g' -e 's%PASS%'"${JENKINS_PASS}"'%g' import_xml.sh > temp.sh
+mv temp.sh import_xml.sh
+
+bash -x import_xml.sh
